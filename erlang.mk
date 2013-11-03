96ac87f5a2f696d9a600a8331720491f43941718
23min <peter@23min.com>
2013-11-03 17:10:00 +0100

    support custom dtl suffix instead of just _dtl

--- erlang.mk
+++ erlang.mk
@@ -92,6 +92,9 @@ ALL_TEST_DEPS_DIRS = $(addprefix $(DEPS_DIR)/,$(TEST_DEPS))
 ERL_LIBS ?= $(DEPS_DIR)
 export ERL_LIBS
 
+DTL_SUFFIX ?= $(DTL_SUFFIX)
+export DTL_SUFFIX
+
 ERLC_OPTS ?= -Werror +debug_info +warn_export_all +warn_export_vars \
 	+warn_shadow_vars +warn_obsolete_guard # +bin_opt_info +warn_missing_spec
 COMPILE_FIRST ?=
